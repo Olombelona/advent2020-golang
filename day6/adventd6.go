b1879549c844ce06eeea0b9871a811b70500d43f
Dinh Doan Van Bien <999472+voieducode@users.noreply.github.com>
2020-12-06 23:48:09 +0000

    count all yes

--- day6/adventd6.go
+++ day6/adventd6.go
@@ -25,24 +25,55 @@ func parseForm(form string) int {
 	return countYes
 }
 
+func parseForm2(form string) int {
+	countAllYes := 0
+
+	var lines = strings.Split(strings.Trim(form, "\n"), "\n")
+	var counts map[rune]int = make(map[rune]int)
+	for _, line := range lines {
+		var seen map[rune]bool = make(map[rune]bool)
+		for _, r := range line {
+			if unicode.IsLetter(r) && !seen[r] {
+				if _, hasCount := counts[r]; hasCount {
+					counts[r]++
+				} else {
+					counts[r] = 1
+				}
+				seen[r] = true
+			}
+		}
+	}
+	numPeople := len(lines)
+	for _, count := range counts {
+		if count == numPeople {
+			countAllYes++
+		}
+	}
+	return countAllYes
+}
+
 func main() {
 	dat, err := ioutil.ReadFile("adventd6-input.txt")
 	check(err)
 	lines := strings.Split(string(dat), "\r\n")
 	countYes := 0
+	countAllYes := 0
 	var form = ""
 	for _, line := range lines {
 		if len(line) == 0 {
 			if form != "" {
 				countYes += parseForm(form)
+				countAllYes += parseForm2(form)
 				form = ""
 			}
 		} else {
-			form += line + " "
+			form += line + "\n"
 		}
 	}
 	if form != "" {
 		countYes += parseForm(form)
+		countAllYes += parseForm2(form)
 	}
 	fmt.Println("Total yes:", countYes)
+	fmt.Println("Total all yes:", countAllYes)
 }
