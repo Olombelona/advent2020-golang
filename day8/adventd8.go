c40803dce1018d3173abc301d788b3f3da636fc5
Dinh Doan Van Bien <999472+voieducode@users.noreply.github.com>
2020-12-12 18:48:19 +0000

    fix and terminate

--- day8/adventd8.go
+++ day8/adventd8.go
@@ -7,6 +7,10 @@ import (
 	"strconv"
 )
 
+
+// Set to true to get debug output
+const verbose = false
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -19,26 +23,31 @@ func extractNum(num string) int {
 	return v
 }
 
-func main() {
-	dat, err := ioutil.ReadFile("adventd8-input.txt")
-	check(err)
-	program := strings.Split(string(dat), "\r\n")
-	accumulator := 0
-	pc := 0
+func canTerminateProgram(program []string, pc int, accumulator int) bool {
 	numberRuns := make([]int, len(program))
+	lastPc := len(program) - 1
+
 	for {
+		if (pc > lastPc) {
+			fmt.Println("Program terminating correctly, acc =", accumulator)
+			return true
+		}
 		line := strings.Split(program[pc], " ")
 		op := line[0]
 		num := 0
-		numberRuns[pc]++
+		
 		if len(line) > 1 {
 			num = extractNum(line[1])
 		} 
-		fmt.Println(pc, op, num, "acc", accumulator)
+
+		numberRuns[pc]++
 		if (numberRuns[pc] > 1) {
-			fmt.Println("Infinite loop detected at", pc, " acc =", accumulator)
-			break
+			if verbose {
+				fmt.Println("  === Infinite loop detected at", pc, " acc =", accumulator)
+			}
+			return false
 		}
+
 		switch op {
 		case "nop":
 			pc++
@@ -51,4 +60,68 @@ func main() {
 			panic("Unknown instruction")
 		}		
 	}
+}
+
+func solve(program []string, fix bool) {
+	accumulator := 0
+	pc := 0
+	numberRuns := make([]int, len(program))
+	lastPc := len(program) - 1
+
+	for {
+		if (pc > lastPc) {
+			fmt.Println("Program terminating correctly, acc =", accumulator)
+			return
+		}
+		line := strings.Split(program[pc], " ")
+		op := line[0]
+		num := 0
+		
+		if len(line) > 1 {
+			num = extractNum(line[1])
+		}
+		if verbose {
+			fmt.Println(pc, op, num, "acc", accumulator)
+		}
+
+		numberRuns[pc]++
+		if (numberRuns[pc] > 1) {
+			fmt.Println("Infinite loop detected at", pc, " acc =", accumulator)
+			break
+		}
+
+		switch op {
+		case "nop":
+			if fix && canTerminateProgram(program, pc + num, accumulator) {
+				// Change to jmp
+				fmt.Println("Changing nop to jmp so code terminates at ", pc)
+				return
+			} 
+			pc++
+			
+		case "acc":
+			accumulator += num
+			pc++
+		case "jmp":
+			if fix && canTerminateProgram(program, pc + 1, accumulator) {
+				// Change to nop
+				fmt.Println("Changing jmp to nop so code terminates at ", pc)
+				return				
+			}
+			pc += num
+			
+		default:
+			panic("Unknown instruction")
+		}		
+	}
+}
+
+func main() {
+	dat, err := ioutil.ReadFile("adventd8-input.txt")
+	check(err)
+	program := strings.Split(string(dat), "\r\n")
+	fmt.Println("== Run 1 - no fix")
+	solve(program, false)
+	fmt.Println("== Run 2 - fix")
+	solve(program, true)
 }
