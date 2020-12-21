8a97a6ed47788246a7430bf3f012c42e68776a25
Dinh Doan Van Bien <999472+voieducode@users.noreply.github.com>
2020-12-21 13:50:04 +0000

    add part2 with waypoint

--- day12/adventd12.go
+++ day12/adventd12.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"strings"
 	"strconv"
+	"math"
 )
 
 // Set to true to get debug output
@@ -14,6 +15,10 @@ const east = 0
 const north = 1
 const west = 2
 const south = 3
+
+const slotLeft = 0
+const slotRight = 1
+
 
 func check(e error) {
 	if e != nil {
@@ -32,28 +37,78 @@ func extractNum(num string) int {
 	return v
 }
 
-func turnShip(facing int, turnBy int) int {
+func extractNumf(num string) float64 {
+	v, err := strconv.ParseFloat(num, 64)
+	check(err)
+	return v
+}
+
+
+func turnShip(facing int, turnBy int, slot int) int {
 	//      N=1
 	//  W=2     E=0
 	//      S=3
 	directions := [][]int{ {north, south}, {west, east}, {south, north}, {east, west} }
 	newFacing := facing	
 	steps := (turnBy / 90)
-	if steps > 0 {
-		for s := 1; s <= steps; s++ {
-			newFacing = directions[newFacing][1]
-		}
-	} else {
-		for s := -1; s >= steps; s-- {
-			newFacing = directions[newFacing][0]
-		}
+	
+	for s := 1; s <= steps; s++ {
+		newFacing = directions[newFacing][slot]
 	}
 
 	return newFacing
 }
 
+func rotateWaypoint(wx float64, wy float64, degrees float64) (float64, float64) {
+	// source: https://stackoverflow.com/a/34374437
+	radians := degrees * (math.Pi/180.0)
+
+	// Assume origin (0, 0)
+	qx := math.Cos(radians) * wx - math.Sin(radians) * wy
+	qy := math.Sin(radians) * wx + math.Cos(radians) * wy
+
+	return  math.Round(qx), math.Round(qy)
+}
+
+func solve2(input string) {
+	fmt.Println("=============== Solving part 2", input)
+	dat, err := ioutil.ReadFile(input)
+	check(err)
+	lines := strings.Split(string(dat), "\r\n")
+	
+	mx := 0.0
+	my := 0.0
+	wx := 10.0
+	wy := 1.0
+	for _, line := range lines {		
+		dir := line[0]
+		v := extractNumf(line[1:])
+		switch dir {
+			case 'N':
+				wy += v
+			case 'S':
+				wy -= v
+			case 'E':
+				wx += v
+			case 'W':
+				wx -= v
+			case 'L':
+				wx, wy = rotateWaypoint(wx, wy, v)
+			case 'R':
+				// Angle sign is different than part 1
+				wx, wy = rotateWaypoint(wx, wy, -v)
+			case 'F':
+				mx += v * wx
+				my += v * wy
+			default:
+				panic("unknow instruction")
+		}
+	}
+	fmt.Println("mx", mx, "my", my, "wx", wx, "wy", wy, "distance",  math.Abs(mx) + math.Abs(my))
+}
+
 func solve(input string) {
-	fmt.Println("=============== Solving", input)
+	fmt.Println("=============== Solving part 1", input)
 	dat, err := ioutil.ReadFile(input)
 	check(err)
 	lines := strings.Split(string(dat), "\r\n")
@@ -74,9 +129,9 @@ func solve(input string) {
 			case 'W':
 				ewPosition -= v
 			case 'L':
-				facing = turnShip(facing, -v)
+				facing = turnShip(facing, v, slotLeft)
 			case 'R':
-				facing = turnShip(facing, v)
+				facing = turnShip(facing, v, slotRight)
 			case 'F':
 				if facing == east {
 					ewPosition += v
@@ -90,11 +145,13 @@ func solve(input string) {
 			default:
 				panic("unknow instruction")
 		}
-		fmt.Println(line, "facing", facing, "ew", ewPosition, "ns", nsPosition, "distance", abs(ewPosition) + abs(nsPosition))
 	}
+	fmt.Println("facing", facing, "ew", ewPosition, "ns", nsPosition, "distance", abs(ewPosition) + abs(nsPosition))
 }
 
 func main() {
 	solve("adventd12-input.txt")
 	solve("adventd12-input-small.txt")
+	solve2("adventd12-input.txt")
+	solve2("adventd12-input-small.txt")
 }
